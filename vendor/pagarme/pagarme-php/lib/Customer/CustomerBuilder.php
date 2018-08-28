ff978f7632ec63c419aa99f4353fdf198da58a6b
irwing max <irwing.1911@gmail.com>
2018-08-28 14:55:18 -0300

    Including composer.json Files for the project dependencies. Like the SDK of Pagar.me, phpCodeSniffer and the use of autoload according to psr4.

---
+++ vendor/pagarme/pagarme-php/lib/Customer/CustomerBuilder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PagarMe\Sdk\Customer;
+
+trait CustomerBuilder
+{
+    /**
+     * @param array $customerData
+     * @return Customer
+     */
+    private function buildCustomer($customerData)
+    {
+        $customerData->address = new Address(
+            get_object_vars($customerData->addresses[0])
+        );
+
+        $customerData->phone = new Phone($customerData->phones[0]);
+
+        $customerData->date_created = new \DateTime(
+            $customerData->date_created
+        );
+
+        return new Customer(get_object_vars($customerData));
+    }
+
+    /**
+     * @param array $customerData
+     * @return Customer
+     */
+    private function buildCustomerFromResponse($customerData, $addressData, $phoneData)
+    {
+        if (is_null($customerData) || $customerData == new \stdClass()) {
+            return null;
+        }
+
+        if (!is_null($addressData)) {
+            $customerData->address = new Address(
+                get_object_vars($addressData)
+            );
+        }
+
+        if (!is_null($phoneData)) {
+            $customerData->phone = new Phone($phoneData);
+        }
+
+        $customerData->date_created = new \DateTime(
+            $customerData->date_created
+        );
+
+        return new Customer(get_object_vars($customerData));
+    }
+}
