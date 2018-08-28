0f38a404531b6b83c936d2a57a8466a1df2d2f0f
irwing max <irwing.1911@gmail.com>
2018-08-28 15:01:30 -0300

    Including RequestData File To bring the data sent by script.js via ajax. It treats the information and separates its proper keys into an array.

---
+++ src/RequestData.php
@@ -0,0 +1,23 @@
+<?php
+namespace Irwing\Pagarme;
+
+class RequestData
+{
+    private $allData;
+    private $individualData;
+
+    public function getIndividualData()
+    {
+        $this->allData = explode(',', $_REQUEST['t']);
+        $this->individualData = array(
+            "cost" => $this->allData[0],
+            "token" => $this->allData[1],
+        );
+
+        for ($indexer = 1; $indexer < count($this->allData) - 1; $indexer++) {
+            $this->individualData["fornecedor" . $indexer] = $this->allData[$indexer+1];
+        }
+
+        return $this->individualData;
+    }
+}
