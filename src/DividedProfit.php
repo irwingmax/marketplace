b67f3a44304941adad0411e4d204b41553b3a675
irwing max <irwing.1911@gmail.com>
2018-08-28 15:02:55 -0300

    Including file to split profits. The purpose of the class is to divide the profits according to the business rule and return so that the transaction can be carried out.

---
+++ src/DividedProfit.php
@@ -0,0 +1,220 @@
+<?php
+namespace Irwing\Pagarme;
+
+class DividedProfit
+{
+    private $objectPagarMe;
+    private $objectRequest;
+    private $objectSplitRules;
+
+    private $totalProfit;
+    private $dividedProfit;
+    private $mariaProfit;
+    private $fee;
+    private $joaoProfit;
+    private $cesarProfit;
+    private $splitRuleMaria;
+    private $splitRuleJoao;
+    private $splitRuleCesar;
+
+
+    public function divide()
+    {
+        define('MARIA_ID', 're_cjl5hnfe200gm466epaay5cqd');
+        define('JOAO_ID', 're_cjl5ic20b00im466efv9o21jt');
+        define('CESAR_ID', 're_cjl5icnop00io466e93gno3ij');
+        define('API', 'ak_test_qDURyncApVQmfq7MCbXrv0ZXjIL0sL');
+
+        define('PRICE_OF_MARIA_PRODUCT', 12500);
+        define('PRICE_OF_JOAO_PRODUCT', 10000);
+        define('PRICE_OF_CESAR_PRODUCT', 15000);
+
+        $this->objectRequest = new \Irwing\Pagarme\RequestData();
+        $this->objectPagarMe = new \PagarMe\Sdk\PagarMe('ak_test_qDURyncApVQmfq7MCbXrv0ZXjIL0sL');
+        $this->objectSplitRules = new \PagarMe\Sdk\SplitRule\SplitRuleCollection();
+
+        $this->totalProfit = $this->objectRequest->getIndividualData()['cost'];
+
+        $this->fee = 4200;
+        $this->dividedProfit = $this->totalProfit - $this->fee;
+
+        if (!array_search('joao', $this->objectRequest->getIndividualData()) &&
+            !array_search('cesar', $this->objectRequest->getIndividualData())) {
+            $this->mariaProfit = $this->totalProfit;
+
+            $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->mariaProfit,
+                $this->objectPagarMe->recipient()->get(MARIA_ID),
+                true,
+                true
+            );
+
+            $this->objectSplitRules[0] = $this->splitRuleMaria;
+            return $this->objectSplitRules;
+        }
+
+        if (!array_search('maria', $this->objectRequest->getIndividualData()) &&
+            !array_search('cesar', $this->objectRequest->getIndividualData())) {
+            $this->mariaProfit = $this->dividedProfit * 0.15;
+            $this->joaoProfit = $this->dividedProfit * 0.85;
+
+            $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->mariaProfit + $this->fee,
+                $this->objectPagarMe->recipient()->get(MARIA_ID),
+                true,
+                true
+            );
+
+            $this->splitRuleJoao = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->joaoProfit,
+                $this->objectPagarMe->recipient()->get(JOAO_ID),
+                true,
+                true
+            );
+
+            $this->objectSplitRules[0] = $this->splitRuleMaria;
+            $this->objectSplitRules[1] = $this->splitRuleJoao;
+
+            return $this->objectSplitRules;
+        }
+
+        if (!array_search('maria', $this->objectRequest->getIndividualData()) &&
+            !array_search('joao', $this->objectRequest->getIndividualData())) {
+            $this->mariaProfit = $this->dividedProfit * 0.15;
+            $this->cesarProfit = $this->dividedProfit * 0.85;
+
+            $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->mariaProfit + $this->fee,
+                $this->objectPagarMe->recipient()->get(MARIA_ID),
+                true,
+                true
+            );
+
+            $this->splitRuleCesar = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->cesarProfit,
+                $this->objectPagarMe->recipient()->get(CESAR_ID),
+                true,
+                true
+            );
+
+            $this->objectSplitRules[0] = $this->splitRuleMaria;
+            $this->objectSplitRules[1] = $this->splitRuleCesar;
+
+            return $this->objectSplitRules;
+        }
+
+        if (!array_search('maria', $this->objectRequest->getIndividualData())) {
+            $this->mariaProfit = PRICE_OF_CESAR_PRODUCT * 0.15;
+            $this->mariaProfit = PRICE_OF_JOAO_PRODUCT * 0.15 + $this->mariaProfit;
+            $this->cesarProfit = PRICE_OF_CESAR_PRODUCT * 0.85;
+            $this->joaoProfit = PRICE_OF_JOAO_PRODUCT * 0.85;
+
+            $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->mariaProfit + $this->fee,
+                $this->objectPagarMe->recipient()->get(MARIA_ID),
+                true,
+                true
+            );
+
+            $this->splitRuleCesar = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->cesarProfit,
+                $this->objectPagarMe->recipient()->get(CESAR_ID),
+                true,
+                true
+            );
+
+            $this->splitRuleJoao = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->joaoProfit,
+                $this->objectPagarMe->recipient()->get(JOAO_ID),
+                true,
+                true
+            );
+
+            $this->objectSplitRules[0] = $this->splitRuleMaria;
+            $this->objectSplitRules[1] = $this->splitRuleCesar;
+            $this->objectSplitRules[2] = $this->splitRuleJoao;
+
+            return $this->objectSplitRules;
+        }
+
+        if (!array_search('joao', $this->objectRequest->getIndividualData())) {
+            $this->mariaProfit = PRICE_OF_CESAR_PRODUCT * 0.15;
+            $this->cesarProfit = PRICE_OF_CESAR_PRODUCT * 0.85;
+
+            $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->mariaProfit + $this->fee + PRICE_OF_MARIA_PRODUCT,
+                $this->objectPagarMe->recipient()->get(MARIA_ID),
+                true,
+                true
+            );
+
+            $this->splitRuleCesar = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->cesarProfit,
+                $this->objectPagarMe->recipient()->get(CESAR_ID),
+                true,
+                true
+            );
+
+            $this->objectSplitRules[0] = $this->splitRuleMaria;
+            $this->objectSplitRules[1] = $this->splitRuleCesar;
+
+            return $this->objectSplitRules;
+        }
+
+        if (!array_search('cesar', $this->objectRequest->getIndividualData())) {
+            $this->mariaProfit = PRICE_OF_JOAO_PRODUCT * 0.15;
+            $this->joaoProfit = PRICE_OF_JOAO_PRODUCT * 0.85;
+
+            $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->mariaProfit + $this->fee + PRICE_OF_MARIA_PRODUCT,
+                $this->objectPagarMe->recipient()->get(MARIA_ID),
+                true,
+                true
+            );
+
+            $this->splitRuleJoao = $this->objectPagarMe->splitRule()->monetaryRule(
+                $this->joaoProfit,
+                $this->objectPagarMe->recipient()->get(JOAO_ID),
+                true,
+                true
+            );
+
+            $this->objectSplitRules[0] = $this->splitRuleMaria;
+            $this->objectSplitRules[1] = $this->splitRuleJoao;
+
+            return $this->objectSplitRules;
+        }
+
+        $this->mariaProfit = PRICE_OF_CESAR_PRODUCT * 0.15;
+        $this->mariaProfit = PRICE_OF_JOAO_PRODUCT * 0.15 + $this->mariaProfit;
+        $this->cesarProfit = PRICE_OF_CESAR_PRODUCT * 0.85;
+        $this->joaoProfit = PRICE_OF_JOAO_PRODUCT * 0.85;
+
+        $this->splitRuleMaria = $this->objectPagarMe->splitRule()->monetaryRule(
+            $this->mariaProfit + $this->fee + PRICE_OF_MARIA_PRODUCT,
+            $this->objectPagarMe->recipient()->get(MARIA_ID),
+            true,
+            true
+        );
+
+        $this->splitRuleCesar = $this->objectPagarMe->splitRule()->monetaryRule(
+            $this->cesarProfit,
+            $this->objectPagarMe->recipient()->get(CESAR_ID),
+            true,
+            true
+        );
+
+        $this->splitRuleJoao = $this->objectPagarMe->splitRule()->monetaryRule(
+            $this->joaoProfit,
+            $this->objectPagarMe->recipient()->get(JOAO_ID),
+            true,
+            true
+        );
+
+        $this->objectSplitRules[0] = $this->splitRuleMaria;
+        $this->objectSplitRules[1] = $this->splitRuleCesar;
+        $this->objectSplitRules[2] = $this->splitRuleJoao;
+
+        return $this->objectSplitRules;
+    }
+}
