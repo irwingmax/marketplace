9f8e9f7d41f5eb5a5eef1e87ff8bed1496caf4ce
irwing max <irwing.1911@gmail.com>
2018-08-29 16:06:30 -0300

    Fixing namespaces declaration, implementing interface and error correction according to psr2

--- src/Transaction.php
+++ src/Transaction.php
@@ -1,7 +1,7 @@
 <?php
 namespace Irwing\Pagarme;
 
-class Transaction
+class Transaction implements \Irwing\Pagarme\Interfaces\TransactionInterface
 {
     private $objectRequest;
     private $objectPagarMe;
@@ -13,21 +13,30 @@ class Transaction
 
     public function finalizeCapture()
     {
-        $this->objectRequest = new \Irwing\Pagarme\RequestData();
+        $this->objectRequest = new RequestData();
         $this->objectPagarMe = new \PagarMe\Sdk\PagarMe('ak_test_qDURyncApVQmfq7MCbXrv0ZXjIL0sL');
-        $this->objectDivideProfit = new \Irwing\Pagarme\DividedProfit();
+        $this->objectDivideProfit = new DividedProfit();
 
         $this->splitrules = $this->objectDivideProfit->divide();
         $this->amountToCapture = $this->objectRequest->getIndividualData()['cost'];
-        $this->transaction = $this->objectPagarMe->transaction()->
-                        get($this->objectRequest->getIndividualData()['token']);
+
+        $this->transaction = $this->objectPagarMe->transaction()->get(
+            $this->objectRequest->getIndividualData()['token']
+        );
+
         $this->metadata = [];
 
-        return $this->objectPagarMe->transaction()->capture(
+        $this->transaction = $this->objectPagarMe->transaction()->capture(
             $this->transaction,
             $this->amountToCapture,
             $this->metadata,
             $this->splitrules
         );
+
+        if ($this->transaction == null) {
+            echo 'Transação não finalizada';
+        }
+
+            echo 'Trasação finalizada com sucesso';
     }
 }
